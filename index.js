fe4250386e797968a1653e40a460858f66f17a73
ItsMoloy <moloyswe20@gmail.com>
2025-01-19 01:06:10 +0600

    GET,POST,UPDATE,DELETE creating done

--- index.js
+++ index.js
@@ -39,6 +39,41 @@ app.post('/api/products', async(req, res) => {
   }
 });
 
+// Update
+
+app.put('/api/products/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if(!product) {
+            return res.status(404).json({message: 'Product not found'});
+        }
+        const updatedProduct = await Product.findById(id);
+
+        res.status(200).json(updatedProduct);
+
+    } catch (err) {
+
+        res.status(500).json({message: err.message});
+    }
+});
+
+app.delete('/api/products/:id', async(req, res) => {
+    try {
+        const id = req.params.id;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product) {
+            return res.status(404).json({message: 'Product not found'});
+        }
+        const deletedProduct = await Product.findById(id);
+        res.status(200).json(deletedProduct);
+
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+}   );
+
 mongoose.connect("mongodb+srv://MOLOY-NODE:mongodb2025@node-api.qoofc.mongodb.net/?retryWrites=true&w=majority&appName=NODE-API")
 
 .then(() => {
