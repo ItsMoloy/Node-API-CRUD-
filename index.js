24b756c3a4e9261e093dca362548dc05b9ed8963
ItsMoloy <moloyswe20@gmail.com>
2025-01-18 23:26:54 +0600

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+mongoose.connect("mongodb+srv://MOLOY-NODE:mongodb2025@node-api.qoofc.mongodb.net/?retryWrites=true&w=majority&appName=NODE-API")
+
+.then(() => {
+    console.log('MongoDB connected');
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000');
+    });
+
+})
+.catch((err) => {
+    console.log('MongoDB connection error', err);
+});
